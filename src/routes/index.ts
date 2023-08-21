76b39dedf70e56d8793ca4dc57d793f3db536c23
Ilya Khamraev <hamraev9517@gmail.com>
2023-08-21 23:59:38 +0500

    create film endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
 
 export const routes = (app: FastifyInstance) => {
   app.get("/user", async (_request, reply) => {
@@ -10,4 +10,44 @@ export const routes = (app: FastifyInstance) => {
       reply.status(500).send(err);
     }
   });
+  app.get("/films", async (_request, reply) => {
+    try {
+      const db = app.mongo.db?.collection("movies");
+      const movies = await db?.find({}).toArray();
+      reply.status(200).send(movies);
+    } catch (err) {
+      reply.status(500).send(err);
+    }
+  });
+  app.post(
+    "/film",
+    async (
+      _request: FastifyRequest<{
+        Body: {
+          promoImg: string;
+          client: string;
+          name: string;
+          category: string;
+          movie: string;
+        };
+      }>,
+      reply
+    ) => {
+      const { promoImg, client, name, category, movie } = _request.body;
+
+      try {
+        const db = app.mongo.db?.collection("movies");
+        const film = await db?.insertOne({
+          promoImg,
+          client,
+          name,
+          category,
+          movie,
+        });
+        reply.status(200).send(film);
+      } catch (err) {
+        reply.status(500).send(err);
+      }
+    }
+  );
 };
